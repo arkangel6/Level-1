4a3b2060301e9a4384e1580cfb7b6165ed1fc0b2
Katelyn-Dragon <league@WTS10.attlocal.net>
2017-01-09 19:58:22 -0800

    1/9/2017

---
+++ src/animal/Giraffffee.java
@@ -0,0 +1,22 @@
+package animal;
+
+public class Giraffffee {
+	public static void main(String[] args) {
+	}
+
+	String favoritePet;
+	int numberOfColors;
+
+	Giraffffee(String favoritePet, int numberOfColors) {
+		this.favoritePet = favoritePet;
+		this.numberOfColors = numberOfColors;
+	}
+
+	public void eat() {
+		System.out.println("good");
+	}
+
+	public void fly() {
+		System.out.println("riding"+ favoritePet);
+	}
+}
---
+++ src/animal/giraffe2.java
@@ -0,0 +1,8 @@
+package animal;
+ 
+public class giraffe2 {
+	public static void main(String[] args) {
+		Giraffffee jerraffe = new Giraffffee(cat,20);
+	}
+
+}
